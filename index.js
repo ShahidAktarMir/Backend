70473194eeccb424ed78b083c945ce8e160e2904
Shahid Aktar Mir <shahidaktarm@gmail.com>
2023-05-20 10:19:28 +0530

    Contact List useing ejs

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
-const http = require("http");
+const express = require("express");
+const path = require("path");
 const port = 8013;
-const fs = require("fs");
+const app = express();
 
-function requestHandeler(req, res) {
-  console.log(req.url);
-  let filePath;
-  switch (req.url) {
-    case "/":
-      filePath = "./home.html";
-      break;
-    case "/profile":
-      filePath = "./404.html";
-    default:
-      filePath = "./404.html";
-  }
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      console.log(err);
-    }
-    return res.end(data);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.set(express.static("assets"));
+
+app.use(express.urlencoded());
+
+var contactList = [
+  {
+    name: "Shahid",
+    phone: "8013203559",
+  },
+];
+
+app.get("/", (req, res) => {
+  return res.render("home", {
+    contact_list: contactList,
   });
-}
-const server = http.createServer(requestHandeler);
-server.listen(port, (err) => {
+});
+app.get("/delete/:phone", (req, res) => {
+  console.log(req.params);
+  let phone = req.params.phone;
+  console.log(phone);
+  let contactIndex = contactList.findIndex((c) => c.phone == phone);
+  if (contactIndex != -1) {
+    contactList.splice(contactIndex, 1);
+  }
+  return res.redirect("/");
+});
+app.post("/add", (req, res) => {
+  console.log(req.body);
+  contactList.push({
+    name: req.body.name,
+    phone: req.body.contact,
+  });
+  return res.redirect("/");
+});
+
+app.listen(port, (err) => {
   err
     ? console.log(err)
-    : console.log("Hi Shahid Server is Now On nad Port:", port);
+    : console.log("Express Server running on Port :", port);
 });
